ff41fbf868c43da31b6888ce9050f9013a0e25ee
Martin Norberg <martinnorberg@MacBook-Pro-som-tillhor-Martin.local>
2020-07-10 11:45:19 +0200

    h alias for handlers

--- backend/pkg/api/routes.go
+++ backend/pkg/api/routes.go
@@ -1,16 +1,16 @@
 package api
 
 import (
-	"backend/pkg/handlers"
+	h "backend/pkg/handlers"
 	mw "backend/pkg/middlewares"
 )
 
 // One routes file for now, but could be split into its own package in the future, and
 // further divided into versions, etc.
 func (a *api) routes() {
-	a.router.HandleFunc("/api/v1/messages", mw.Logger(handlers.GetMessages)).Methods("GET")
-	a.router.HandleFunc("/api/v1/messages", mw.Logger(handlers.CreateMessage)).Methods("POST")
-	a.router.HandleFunc("/api/v1/messages/{id:[0-9]+}", mw.Logger(handlers.GetMessage)).Methods("GET")
-	a.router.HandleFunc("/api/v1/messages/{id:[0-9]+}", mw.Logger(handlers.UpdateMessage)).Methods("PUT")
-	a.router.HandleFunc("/api/v1/messages/{id:[0-9]+}", mw.Logger(handlers.DeleteMessage)).Methods("DELETE")
+	a.router.HandleFunc("/api/v1/messages", mw.Logger(h.GetMessages)).Methods("GET")
+	a.router.HandleFunc("/api/v1/messages", mw.Logger(h.CreateMessage)).Methods("POST")
+	a.router.HandleFunc("/api/v1/messages/{id:[0-9]+}", mw.Logger(h.GetMessage)).Methods("GET")
+	a.router.HandleFunc("/api/v1/messages/{id:[0-9]+}", mw.Logger(h.UpdateMessage)).Methods("PUT")
+	a.router.HandleFunc("/api/v1/messages/{id:[0-9]+}", mw.Logger(h.DeleteMessage)).Methods("DELETE")
 }
